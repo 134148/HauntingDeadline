8567709dd1b648f5dd1ed5b6f21f95ff8d182361
134148 <134148@stanislascollege.net>
2026-02-14 20:19:09 +0100

    init

---
+++ Basisgame.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+# Declare member variables here.
+var player
+var enemies
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_enemies()
+	get_player_details()
+
+func add_enemies():
+	pass # Add code to do this later
+
+func get_player_details():
+	pass # Add the code later
+
+# Called every frame.
+func _process(delta):
+	process_inputs(delta)
+	process_enemy_activity(delta)
+	update_score()
+
+func process_inputs(delta):
+	pass
+
+func process_enemy_activity(delta):
+	pass
+
+func update_score():
+	pass
