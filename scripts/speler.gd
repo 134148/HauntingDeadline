b361f1f45445f2b490b5b496f6d75fc26e9ac445
134148 <134148@stanislascollege.net>
2026-03-05 14:58:04 +0100

    Shirt en loop nog niet af

--- scripts/speler.gd
+++ scripts/speler.gd
@@ -4,30 +4,34 @@ extends CharacterBody2D
 var character_direction : Vector2
 
 func _physics_process(delta):
-	character_direction.x = Input.get_axis("left","right")
-	character_direction.y = Input.get_axis("up","down")
+
+	character_direction.x = Input.get_axis("left", "right")
+	character_direction.y = Input.get_axis("up", "down")
 	character_direction = character_direction.normalized()
-	
-	#flip
-	if character_direction.x > 0: %animatie_speler.flip_h = false
-	elif character_direction.x < 0: %animatie_speler.flip_h = true
-	
-	if character_direction:
+
+	# Flip sprites
+	if character_direction.x > 0:
+		%animatie_speler.flip_h = false
+		%animatie_haar.flip_h = false
+	elif character_direction.x < 0:
+		%animatie_speler.flip_h = true
+		%animatie_haar.flip_h = true
+
+	# Movement
+	if character_direction != Vector2.ZERO:
 		velocity = character_direction * movement_speed
-		if %animatie_speler.animation != "Walking": %animatie_speler.animation = "Walking"
+		
+		if %animatie_speler.animation != "Walking":
+			%animatie_speler.animation = "Walking"
+		if %animatie_haar.animation != "hair_walking":
+			%animatie_haar.play("hair_walking")
+
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
-		if %animatie_speler.animation != "Idle": %animatie_speler.animation = "Idle"
 		
-	#flip haar
-	if character_direction.x > 0: %animatie_haar.flip_h = false
-	elif character_direction.x < 0: %animatie_haar.flip_h = true
-	
-	if character_direction:
-		velocity = character_direction * movement_speed
-		if %animatie_haar.animation != "hair_walking": %animatie_haar.animation = "hair_walking"
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
-		if %animatie_haar.animation != "hair_idle": %animatie_haar.animation = "hair_idle"
-		
+		if %animatie_speler.animation != "Idle":
+			%animatie_speler.animation = "Idle"
+		if %animatie_haar.animation != "hair_idle":
+			%animatie_haar.animation = "hair_idle"
+
 	move_and_slide()
