e91895ba70f9671ae1e8f559388d9888cc6a2bf7
134148 <134148@stanislascollege.net>
2026-03-05 17:49:00 +0100

    alle kleren

--- scripts/speler.gd
+++ scripts/speler.gd
@@ -13,9 +13,15 @@ func _physics_process(delta):
 	if character_direction.x > 0:
 		%animatie_speler.flip_h = false
 		%animatie_haar.flip_h = false
+		%animatie_shirt.flip_h = false
+		%animatie_broek.flip_h = false
+		%animatie_schoenen.flip_h = false
 	elif character_direction.x < 0:
 		%animatie_speler.flip_h = true
 		%animatie_haar.flip_h = true
+		%animatie_shirt.flip_h = true
+		%animatie_broek.flip_h = true
+		%animatie_schoenen.flip_h = true
 
 	# Movement
 	if character_direction != Vector2.ZERO:
@@ -25,6 +31,12 @@ func _physics_process(delta):
 			%animatie_speler.animation = "Walking"
 		if %animatie_haar.animation != "hair_walking":
 			%animatie_haar.play("hair_walking")
+		if %animatie_shirt.animation != "shirt_walking":
+			%animatie_shirt.play("shirt_walking")
+		if %animatie_broek.animation != "broek_walking":
+			%animatie_broek.play("broek_walking")
+		if %animatie_schoenen.animation != "schoenen_walking":
+			%animatie_schoenen.play("schoenen_walking")
 
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
@@ -33,5 +45,11 @@ func _physics_process(delta):
 			%animatie_speler.animation = "Idle"
 		if %animatie_haar.animation != "hair_idle":
 			%animatie_haar.animation = "hair_idle"
+		if %animatie_shirt.animation != "shirt_idle":
+			%animatie_shirt.animation = "shirt_idle"
+		if %animatie_broek.animation != "broek_idle":
+			%animatie_broek.animation = "broek_idle"
+		if %animatie_schoenen.animation != "schoenen_idle":
+			%animatie_schoenen.animation = "schoenen_idle"
 
 	move_and_slide()
