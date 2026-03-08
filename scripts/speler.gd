d7b88c0415d87f38f779801f9bdf7664928ce5d8
Daria <134402@stanislascollege.net>
2026-03-08 17:30:14 +0100

    robert naar comp en verschillende karakters

--- scripts/speler.gd
+++ scripts/speler.gd
@@ -11,45 +11,45 @@ func _physics_process(delta):
 
 	# Flip sprites
 	if character_direction.x > 0:
-		%animatie_speler.flip_h = false
-		%animatie_haar.flip_h = false
-		%animatie_shirt.flip_h = false
-		%animatie_broek.flip_h = false
-		%animatie_schoenen.flip_h = false
+		%animatie_robert.flip_h = false
+		%animatie_haar_r.flip_h = false
+		%animatie_shirt_r.flip_h = false
+		%animatie_broek_r.flip_h = false
+		%animatie_schoenen_r.flip_h = false
 	elif character_direction.x < 0:
-		%animatie_speler.flip_h = true
-		%animatie_haar.flip_h = true
-		%animatie_shirt.flip_h = true
-		%animatie_broek.flip_h = true
-		%animatie_schoenen.flip_h = true
+		%animatie_robert.flip_h = true
+		%animatie_haar_r.flip_h = true
+		%animatie_shirt_r.flip_h = true
+		%animatie_broek_r.flip_h = true
+		%animatie_schoenen_r.flip_h = true
 
 	# Movement
 	if character_direction != Vector2.ZERO:
 		velocity = character_direction * movement_speed
 		
-		if %animatie_speler.animation != "Walking":
-			%animatie_speler.animation = "Walking"
-		if %animatie_haar.animation != "hair_walking":
-			%animatie_haar.play("hair_walking")
-		if %animatie_shirt.animation != "shirt_walking":
-			%animatie_shirt.play("shirt_walking")
-		if %animatie_broek.animation != "broek_walking":
-			%animatie_broek.play("broek_walking")
-		if %animatie_schoenen.animation != "schoenen_walking":
-			%animatie_schoenen.play("schoenen_walking")
+		if %animatie_robert.animation != "Walking":
+			%animatie_robert.animation = "Walking"
+		if %animatie_haar_r.animation != "hair_walking":
+			%animatie_haar_r.play("hair_walking")
+		if %animatie_shirt_r.animation != "shirt_walking":
+			%animatie_shirt_r.play("shirt_walking")
+		if %animatie_broek_r.animation != "broek_walking":
+			%animatie_broek_r.play("broek_walking")
+		if %animatie_schoenen_r.animation != "schoenen_walking":
+			%animatie_schoenen_r.play("schoenen_walking")
 
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 		
-		if %animatie_speler.animation != "Idle":
-			%animatie_speler.animation = "Idle"
-		if %animatie_haar.animation != "hair_idle":
-			%animatie_haar.animation = "hair_idle"
-		if %animatie_shirt.animation != "shirt_idle":
-			%animatie_shirt.animation = "shirt_idle"
-		if %animatie_broek.animation != "broek_idle":
-			%animatie_broek.animation = "broek_idle"
-		if %animatie_schoenen.animation != "schoenen_idle":
-			%animatie_schoenen.animation = "schoenen_idle"
+		if %animatie_robert.animation != "Idle":
+			%animatie_robert.animation = "Idle"
+		if %animatie_haar_r.animation != "hair_idle":
+			%animatie_haar_r.animation = "hair_idle"
+		if %animatie_shirt_r.animation != "shirt_idle":
+			%animatie_shirt_r.animation = "shirt_idle"
+		if %animatie_broek_r.animation != "broek_idle":
+			%animatie_broek_r.animation = "broek_idle"
+		if %animatie_schoenen_r.animation != "schoenen_idle":
+			%animatie_schoenen_r.animation = "schoenen_idle"
 
 	move_and_slide()
