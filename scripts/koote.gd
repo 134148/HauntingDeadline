405c6597f93850fe75fe89ada4e467c4ee3a3a6e
Daria <134402@stanislascollege.net>
2026-03-09 18:17:29 +0100

    tragisch gefaald met een reactie op gesloten deur, er gebeurt niets, maar kom denk later wel op terug

--- scripts/koote.gd
+++ scripts/koote.gd
@@ -1,1 +1,28 @@
 extends CharacterBody2D
+
+var speed = 25
+var player_chase = true
+var player = null  # node reference
+
+@onready var text_display = %TheTekst
+
+func _ready():
+	var scene_root = get_tree().current_scene
+	
+	# check direct children
+	if scene_root.has_node("Robert"):
+		player = scene_root.get_node("Robert")
+	elif scene_root.has_node("Kiara"):
+		player = scene_root.get_node("Kiara")
+	elif scene_root.has_node("Daria"):
+		player = scene_root.get_node("Daria")
+		
+func _physics_process(delta: float) -> void:
+	if text_display.dialogue_active:
+		return
+	if player_chase and player != null:
+		# richting naar speler
+		var direction = (player.position - position).normalized()
+		# beweging
+		position += direction * speed * delta
+		move_and_collide(Vector2(0,0))
--- scripts/robert_terug.gd
+++ scripts/robert_terug.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+var uren = 23
+var minuten = 30
+var speed = 1.0
+var minute_accumulator = 0.0 
+var time_up = false
+var deur_text = false
+@onready var text_display = %TheTekst
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,5 +25,33 @@ func _on_area_2d_body_exited(body):
 		player_in_area = false
 
 func _process(delta):
+	if text_display.dialogue_active:
+		return
+	minute_accumulator += delta * speed
+	if minute_accumulator >= 1.0:
+		var add_minutes = int(minute_accumulator)
+		minuten += add_minutes
+		minute_accumulator -=add_minutes
+		if minuten >= 60:
+			uren = "00"
+			minuten = "00"
+			get_tree().reload_current_scene()
+	$CanvasLayer/TijdDisplay/TijdText.text = str(uren) + ':' + str(minuten)
+	
+	if deur_text == true:
+		$CanvasLayer/TextDisplay.show()
+		text_display.dialogue_active = true
+		$CanvasLayer/TextDisplay/TheTekst.text = "Oh nee de deur is dicht!"
+		if Input.is_action_just_pressed("space"):
+			%TextDisplay.hide()
+			text_display.dialogue_active = false
+			deur_text = false
+	
 	if player_in_area and Input.is_action_just_pressed("space"):
 		get_tree().change_scene_to_file("res://scenes/robert_terug.tscn")
+		
+
+
+func _on_deur_tekst_body_entered(body: Node2D) -> void:
+	if body.name == "Robert":
+		deur_text = true
