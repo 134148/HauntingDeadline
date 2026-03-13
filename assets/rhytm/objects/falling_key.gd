887a691d36b05e9ae861ca714fac135a181de71c
Robert8862 <134180@stanislascollege.net>
2026-03-13 01:23:20 +0100

    Verder gewerkt aan Rhytm

---
+++ assets/rhytm/objects/falling_key.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+
+@export var fall_speed: float = 3.0
+
+var init_y_pos: float = -360
+
+func _init():
+	set_process(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	global_position += Vector2(0, fall_speed)
+	
+	#Find out how long it takes for arrow to reach critical spot, in my case 3.6
+	if global_position.y > 290 and not $Timer.is_stopped():
+		print($Timer.wait_time - $Timer.time_left)
+		$Timer.stop()
+
+func Setup(target_x: float):
+	global_position = Vector2(target_x,init_y_pos)
+	set_process(true)
+	
---
+++ assets/rhytm/objects/keylistener.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var falling_key = preload("res://assets/rhytm/objects/falling_key.tscn")
+@export var key_name: String = ""
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed(key_name):
+		CreateFallingKey()
+
+func CreateFallingKey():
+	var fk_inst = falling_key.instantiate()
+	get_tree().get_root().call_deferred("add_child", fk_inst)
+	fk_inst.Setup(position.x)
