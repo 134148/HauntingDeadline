398ac4e35c37733484c030d979f0cd69e4aa26cc
Robert8862 <134180@stanislascollege.net>
2026-03-13 21:55:39 +0100

    verder
    bijna

--- assets/rhytm/objects/keylistener.gd
+++ assets/rhytm/objects/keylistener.gd
@@ -22,16 +22,8 @@ var ok_press_score: float = 20
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if falling_key_queue.size() > 0: 
-		
-		
 		if falling_key_queue.front().has_passed:
 			falling_key_queue.pop_front()
-			
-			var st_inst = score_text.instantiate() 
-			get_tree().get_root().call_deferred("add_child", st_inst)
-			st_inst.SetTextInfo("MISS!")
-			st_inst.global_position = global_position + Vector2(0, -15) 
-
 	
 		if Input.is_action_just_pressed(key_name):
 			var key_to_pop = falling_key_queue.pop_front()
@@ -42,23 +34,35 @@ func _process(delta: float) -> void:
 			if distance_from_pass < perfect_press_threshold: 
 				Signals.IncrementScore.emit(perfect_press_threshold)
 				press_score_text = "PERFECT!"
+				Signals.IncrementCombo.emit()
 			elif distance_from_pass < great_press_threshold: 
 				Signals.IncrementScore.emit(great_press_threshold)
 				press_score_text = "GREAT!"
+				Signals.IncrementCombo.emit()
 			elif distance_from_pass < good_press_threshold: 
 				Signals.IncrementScore.emit(good_press_threshold)
 				press_score_text = "GOOD!"
+				Signals.IncrementCombo.emit()
 			elif distance_from_pass < ok_press_threshold: 
 				Signals.IncrementScore.emit(ok_press_threshold)
 				press_score_text = "OK!"
+				Signals.IncrementCombo.emit()
 			else:
 				press_score_text = "MISS!"
+				Signals.ResetCombo.emit()
+				
 				pass
 
 
 			key_to_pop.queue_free()
 			
-			
+			var st_inst = score_text.instantiate() 
+			get_tree().get_root().call_deferred("add_child", st_inst)
+			st_inst.SetTextInfo(press_score_text)
+			st_inst.global_position = global_position + Vector2(0, -15) 
+			Signals.ResetCombo.emit()
+
+
 func CreateFallingKey():
 	var fk_inst = falling_key.instantiate()
 	get_tree().get_root().call_deferred("add_child", fk_inst)
